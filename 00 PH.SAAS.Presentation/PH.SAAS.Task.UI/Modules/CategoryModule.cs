0321602fdfcd2dac75aff40318e39eaf7ed41c80
datetime2 <kozo.wo1aspx@gmail.com>
2016-12-07 17:42:55 +0800

    todo

--- 00 PH.SAAS.Presentation/PH.SAAS.Task.UI/Modules/CategoryModule.cs
+++ 00 PH.SAAS.Presentation/PH.SAAS.Task.UI/Modules/CategoryModule.cs
@@ -4,15 +4,18 @@ using Nancy.ModelBinding;
 using PH.SAAS.Task.Data.Dao;
 using PH.SAAS.Task.Models;
 using PH.SAAS.Task.Models.QueryModel;
+using PH.SAAS.Task.Models.ViewModel;
 
 namespace PH.SAAS.Task.UI.Modules
 {
-    public class CategoryModule:BaseModule
+    public class CategoryModule : BaseModule
     {
-        public CategoryModule():base("Category")
+        public CategoryModule()
+            : base("Category")
         {
             Get["/"] = parameters => View["Index"];
-            Get["/Form"] = parameters => View["Form"];
+            Get["/Form/"] = parameters => View["Form"];
+            Get["/Form?keyValue={keyValue:int}"] = parameters => View["Form"];
             Get["/List"] = paramsters =>
             {
                 var query = this.Bind<jqGridBaseQueryModel>();
@@ -23,7 +26,13 @@ namespace PH.SAAS.Task.UI.Modules
             {
                 var cate = this.Bind<t_Category>();
                 var cateService = new Category();
-                return cateService.SaveForm(cate)?Success("操作成功"):Error("操作失败");
+                return cateService.SaveForm(cate) ? Success("操作成功") : Error("操作失败");
+            };
+            Get["/InitForm"] = paramsters =>
+            {
+                var post = this.Bind<EditPostForm>();
+                var cateService = new Category();
+                return Response.AsJson(cateService.InitForm(post.keyValue));
             };
         }
     }
---
+++ 01 PH.SAAS.Data/PH.SAAS.Task.Models/ViewModel/EditPostForm.cs
@@ -0,0 +1,10 @@
+﻿namespace PH.SAAS.Task.Models.ViewModel
+{
+    public class EditPostForm
+    {
+        /// <summary>
+        /// 主键标识
+        /// </summary>
+        public int keyValue { get; set; } 
+    }
+}
